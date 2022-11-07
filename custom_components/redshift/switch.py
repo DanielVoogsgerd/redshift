9d14a6c6157950b520777c56bda9ab5b8a25b590
Daniel Voogsgerd <daniel@voogsgerd.nl>
2022-11-07 15:48:21 +0100

    First working version

--- custom_components/redshift/switch.py
+++ custom_components/redshift/switch.py
@@ -89,20 +89,6 @@ PLATFORM_SCHEMA = vol.Schema(
 )
 
 
-async def async_set_lights_xy(hass, lights, x_val, y_val, brightness, transition):
-    """Set color of array of lights."""
-    for light in lights:
-        if is_on(hass, light):
-            service_data = {ATTR_ENTITY_ID: light}
-            if x_val is not None and y_val is not None:
-                service_data[ATTR_XY_COLOR] = [x_val, y_val]
-            if brightness is not None:
-                service_data[ATTR_BRIGHTNESS] = brightness
-            if transition is not None:
-                service_data[ATTR_TRANSITION] = transition
-            await hass.services.async_call(LIGHT_DOMAIN, SERVICE_TURN_ON, service_data)
-
-
 async def async_set_lights_temp(hass, lights, mired, brightness, transition):
     """Set color of array of lights."""
     for light in lights:
@@ -112,18 +98,6 @@ async def async_set_lights_temp(hass, lights, mired, brightness, transition):
                 service_data[ATTR_COLOR_TEMP] = int(mired)
             if brightness is not None:
                 service_data[ATTR_BRIGHTNESS] = brightness
-            if transition is not None:
-                service_data[ATTR_TRANSITION] = transition
-            await hass.services.async_call(LIGHT_DOMAIN, SERVICE_TURN_ON, service_data)
-
-
-async def async_set_lights_rgb(hass, lights, rgb, transition):
-    """Set color of array of lights."""
-    for light in lights:
-        if is_on(hass, light):
-            service_data = {ATTR_ENTITY_ID: light}
-            if rgb is not None:
-                service_data[ATTR_RGB_COLOR] = rgb
             if transition is not None:
                 service_data[ATTR_TRANSITION] = transition
             await hass.services.async_call(LIGHT_DOMAIN, SERVICE_TURN_ON, service_data)
@@ -191,13 +165,13 @@ class RedshiftSwitch(SwitchEntity, RestoreEntity):
 
     def parse_setpoints(self, config_setpoints):
         """Parse setpoints from config into a dictionary"""
-        from collections import DefaultDict
-        setpoints = DefaultDict(dict)
+        from collections import defaultdict
+        setpoints = defaultdict(lambda: defaultdict(lambda: (None, None)))
         for setpoint in config_setpoints:
-            lights = setpoint["lights"] if setpoint["lights"] else self._lights
+            lights = setpoint["lights"] if "lights" in setpoint else self._lights
             time = setpoint["time"]
-            brightness = setpoint["brightness"] if setpoint["brightness"] else setpoints[light][time][0]
-            temperature = setpoint["temperature"] if setpoint["temperature"] else setpoints[light][time][1]
+            brightness = setpoint["brightness"] if "brightness" in setpoint else setpoints[light][time][0]
+            temperature = setpoint["temperature"] if "temperature" in setpoint else setpoints[light][time][1]
             for light in lights:
                 setpoints[light][time] = (brightness, temperature)
 
@@ -247,6 +221,9 @@ class RedshiftSwitch(SwitchEntity, RestoreEntity):
 
         self.async_write_ha_state()
 
+    def time_to_datetime(self, now, time):
+        return now.replace(hour=time.hour, minute=time.minute, second=0)
+
     async def async_redshift_update(self, utcnow=None):
         """Update all lights using redshift."""
         if utcnow is None:
@@ -254,7 +231,8 @@ class RedshiftSwitch(SwitchEntity, RestoreEntity):
 
         now = as_local(utcnow)
 
-        if now < self._start_time or now > self._end_time:
+        if now < self.time_to_datetime(now, self._start_time) or now > self.time_to_datetime(now, self._end_time):
+            _LOGGER.debug("Redshift is currenly disabled")
             return
 
         for light, states in self._setpoints.items():
@@ -269,6 +247,7 @@ class RedshiftSwitch(SwitchEntity, RestoreEntity):
 
             # find setpoints before and after now
             for time, (brightness, temperature) in states:
+                time = self.time_to_datetime(now, time)
                 if time < now:
                     if brightness is not None:
                         brightness_start_time = time
@@ -302,7 +281,7 @@ class RedshiftSwitch(SwitchEntity, RestoreEntity):
 
             mired = color_temperature_kelvin_to_mired(temperature)
             await async_set_lights_temp(
-                self.hass, light, mired, brightness, self._transition
+                self.hass, [light], mired, brightness, self._transition
             )
             _LOGGER.debug(
                 "Light %s updated to mired:%s brightness:%s at %s",
@@ -311,122 +290,3 @@ class RedshiftSwitch(SwitchEntity, RestoreEntity):
                 brightness,
                 now,
             )
-
-    async def async_redshift_update_old(self, utcnow=None):
-        """Update all the lights using redshift."""
-        if utcnow is None:
-            utcnow = dt_utcnow()
-
-        now = as_local(utcnow)
-
-        sunset = get_astral_event_date(self.hass, SUN_EVENT_SUNSET, now.date())
-        start_time = self.find_start_time(now)
-        end_time = self.find_end_time(now)
-
-        if end_time <= start_time:
-            # stop_time does not happen in the same day as start_time
-            if start_time < now:
-                # stop time is tomorrow
-                end_time += datetime.timedelta(days=1)
-        elif now < start_time:
-            # stop_time was yesterday since the new start_time is not reached
-            end_time -= datetime.timedelta(days=1)
-
-        if start_time < now < sunset:
-            # Daytime
-            time_state = "day"
-            temp_range = abs(self._start_colortemp - self._sunset_colortemp)
-            day_length = int(sunset.timestamp() - start_time.timestamp())
-            seconds_from_start = int(now.timestamp() - start_time.timestamp())
-            percentage_complete = seconds_from_start / day_length
-            temp_offset = temp_range * percentage_complete
-            if self._start_colortemp > self._sunset_colortemp:
-                temp = self._start_colortemp - temp_offset
-            else:
-                temp = self._start_colortemp + temp_offset
-        else:
-            # Night time
-            time_state = "night"
-
-            if now < end_time:
-                if end_time < start_time and end_time.day == sunset.day:
-                    # we need to use yesterday's sunset time
-                    sunset_time = sunset - datetime.timedelta(days=1)
-                else:
-                    sunset_time = sunset
-
-                night_length = int(end_time.timestamp() - sunset_time.timestamp())
-                seconds_from_sunset = int(now.timestamp() - sunset_time.timestamp())
-                percentage_complete = seconds_from_sunset / night_length
-            else:
-                percentage_complete = 1
-
-            temp_range = abs(self._sunset_colortemp - self._stop_colortemp)
-            temp_offset = temp_range * percentage_complete
-            if self._sunset_colortemp > self._stop_colortemp:
-                temp = self._sunset_colortemp - temp_offset
-            else:
-                temp = self._sunset_colortemp + temp_offset
-        rgb = color_temperature_to_rgb(temp)
-        x_val, y_val, b_val = color_RGB_to_xy_brightness(*rgb)
-        brightness = self._brightness if self._brightness else b_val
-        if self._disable_brightness_adjust:
-            brightness = None
-        if self._mode == MODE_XY:
-            await async_set_lights_xy(
-                self.hass, self._lights, x_val, y_val, brightness, self._transition
-            )
-            _LOGGER.debug(
-                "Lights updated to x:%s y:%s brightness:%s, %s%% "
-                "of %s cycle complete at %s",
-                x_val,
-                y_val,
-                brightness,
-                round(percentage_complete * 100),
-                time_state,
-                now,
-            )
-        elif self._mode == MODE_RGB:
-            await async_set_lights_rgb(self.hass, self._lights, rgb, self._transition)
-            _LOGGER.debug(
-                "Lights updated to rgb:%s, %s%% of %s cycle complete at %s",
-                rgb,
-                round(percentage_complete * 100),
-                time_state,
-                now,
-            )
-        else:
-            # Convert to mired and clamp to allowed values
-            mired = color_temperature_kelvin_to_mired(temp)
-            await async_set_lights_temp(
-                self.hass, self._lights, mired, brightness, self._transition
-            )
-            _LOGGER.debug(
-                "Lights updated to mired:%s brightness:%s, %s%% "
-                "of %s cycle complete at %s",
-                mired,
-                brightness,
-                round(percentage_complete * 100),
-                time_state,
-                now,
-            )
-
-    def find_start_time(self, now):
-        """Return sunrise or start_time if given."""
-        if self._start_time:
-            sunrise = now.replace(
-                hour=self._start_time.hour, minute=self._start_time.minute, second=0
-            )
-        else:
-            sunrise = get_astral_event_date(self.hass, SUN_EVENT_SUNRISE, now.date())
-        return sunrise
-
-    def find_end_time(self, now):
-        """Return dusk or stop_time if given."""
-        if self._end_time:
-            dusk = now.replace(
-                hour=self._end_time.hour, minute=self._end_time.minute, second=0
-            )
-        else:
-            dusk = get_astral_event_date(self.hass, "dusk", now.date())
-        return dusk
